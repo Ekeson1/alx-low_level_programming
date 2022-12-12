08d1d271382c849199f384486a2835092029c2cf
Ndubueze Eke <ekesonmercy@gmail.com>
2022-12-11 17:21:14 -0800

    Positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,11 +14,17 @@ int main(void)
 	strnd(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
+	{
 		printf("%i is positive\n", n);
+	}
 	else if (n < 0)
+	{
 		printf("%i is negative\n", n);
+	}
 	else
+	{
 		printf("%i is zero\n", n);
+	}
 	return (0);
 }
 #
