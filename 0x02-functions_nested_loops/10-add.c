95c0689fbc408455624ca493d3b80f16bb8acdf7
Ndubueze Eke <ekesonmercy@gmail.com>
2022-12-14 02:41:33 -0800

    Add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,9 +4,9 @@
  * add - add two numbers
  * @a: the first number
  * @b: the second number
- * Return: the addition of both
+ * Return: the addition of the addition
  */
 int add(int a, int b)
 {
-	return (a - b);
+	return (a + b);
 }
