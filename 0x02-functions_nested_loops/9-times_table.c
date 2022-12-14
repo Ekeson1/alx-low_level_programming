edfe9a60eb05184d97233200d14e10d9999c5027
Ndubueze Eke <ekesonmercy@gmail.com>
2022-12-14 02:33:03 -0800

    Time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,17 +3,17 @@
  /**
   * times_table - Prints the 9 times table, starting with 0.
   */
- void times_table(void)
- {
- 	int num, mult, prod;
-
- 	for (num = 0; num <= 9; num++)
- 	{
- 		_putchar('0');
-
- 		for (mult = 1; mult <= 9; mult++)
- 		{
- 			_putchar(',');
+void times_table(void)
+{
+	int num, mult, prod;
+	
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+		
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
  			_putchar(' ');
 
  			prod = num * mult;
