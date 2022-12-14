6f4575478e9359b0c93ce4940365a6e683cb20ae
Ndubueze Eke <ekesonmercy@gmail.com>
2022-12-14 01:30:19 -0800

    Hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,8 +2,7 @@
 
 /**
  * jack_bauer - prints every minute of the day of
- * Jack Bauer, starting from 00:00 to 23:59
- * Return: Always 0
+ *              Jack Bauer, starting from 00:00 to 23:59.
  */
 void jack_buaer(void)
 {
