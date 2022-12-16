fba4b686ef0264555fea5f661d32cbcb57ddce6d
Ndubueze Eke <ekesonmercy@gmail.com>
2022-12-15 22:56:52 -0800

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,12 +13,14 @@ int main(void)
 
 	i = 0;
 
-	/*
-	 * while (i < 10)
-	 * {
-	 * putchar(i);
-	 * }
-	 */
-	printf("infinite loop avided! \\o/\n");
+	
+	/* while (i < 10)*/
+	/* {*/
+	/* putchar(i);*/
+       	/* }*/
+	 
+	printf("Infinite loop avioded! \\o/\n");
+
 	return (0);
+
 }
