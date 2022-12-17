0e7089c7fe42f689cda6ba87bc926d618c26e4dc
Ndubueze Eke <ekesonmercy@gmail.com>
2022-12-16 19:34:01 -0800

    Task 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,15 +6,17 @@
   * Return: always 0
   **/
 
- int main(void)
+int main(void);
 {
- 	unsigned long int i, n = 612852475143;
+	unsigned long int i, n = 612852475143;
 
- 	for (i = 3; i < 782849; i = i + 2)
- 	{
- 		while ((n % i == 0) && (n != i))
- 			n = n / i;
- 	}
- 	printf("%lu\n", n);
- 	return (0);
+
+	for (i = 3; i < 782849; i = i + 2)
+	{
+		while
+			((n % i == 0) && (n != i))
+			n = n / i;
+	}
+	printf("%lu\n", n);
+	return (0);
 }
