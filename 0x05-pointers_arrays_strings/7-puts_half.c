7a5496357cdc2e758844aacb30b36c63a1f0f6c5
Ndubueze Eke <ekesonmercy@gmail.com>
2022-12-19 13:48:25 -0800

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,13 +5,13 @@
  * Description: If add number of chars, print (length - 1) /2
  */
 
-void puts_half(char "str")
+void puts_half(char)
 {
 	int i;
 	for (i = 0; str[i] != '\0'; i++)
 		;
 	i++;
-	for (i /= 2; str[i] != '\0\; i++)
+	for (i /= 2; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
