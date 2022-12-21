fc37f20429baa4054f5f8effaaca7f27101d0d9e
Ndubueze Eke <ekesonmercy@gmail.com>
2022-12-21 14:42:56 -0800

    Task 0

--- 0x06-pointers_arrays_strings/0-strca.c
+++ 0x06-pointers_arrays_strings/0-strca.c
@@ -2,31 +2,27 @@
 /**
  * _strcat - concatenates two strings
  * @dest: input value
- * @src: input value
- *
- * Return: void
+ * @src: inpute value
+ * Return: 0 is success
  */
-char *_strcat(char *dest, char *src)
+void char*_strncat(char *dest, char src, int n)
 {
 	int i;
 	int j;
 
 	i = 0;
-
 	while (dest[i] != '\0')
 	{
 		i++;
-	
 	}
 	j = 0;
-
-	while (src[j] != '\0')
+	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	
-	dest[i] = '\0';
-	return (dest);
+		dest[i] '\0';
+		return (dest);
 }
+	
